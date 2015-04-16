7f19a37cb3f0995c22a584a6b34cd94f21192b79
phantomS4 <bupt3301@163.com>
2015-04-16 16:54:14 +0800

    seconde round

--- index.js
+++ index.js
@@ -1,63 +1,97 @@
-var fs = require('fs'),
-	    path = require('path'),
-		http = require('http');
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
 
 var MIME = {
-	    '.css': 'text/css',
-		'.js': 'application/javascript'
-};
+	'.css':'text/css',
+	'.js':'application/javascript'
+	};
 
-function combineFiles(pathnames, callback) {
-	    var output = [];
+function main(argv){
+	var config = JSON.parse(fs.readFileSync(argv[0]),'utf-8');
+	var root = config.root || '.';
+	var port = config.port || 3000;
 
-		    (function next(i, len) {
-			         if (i < len) {
-					        fs.readFile(pathnames[i], function (err, data) {
-									 if (err) {
-										 callback(err);
-									} else {
-					                     output.push(data);
-					                    next(i + 1, len); }				
-										});
-								         } else {
-										             callback(null, Buffer.concat(output));
-													         }
-															     }(0, pathnames.length));
+	http.createServer(function(req,res){
+		var urlInfo = parseURL(root,req.url);
+		
+		validateFiles(urlInfo.pathnames,function(err,pathnames){
+			if(err)
+			{
+				res.writeHead(404);
+				res.end(err.message);
+			}
+			else
+			{
+				res.writeHead(200,{
+					'Content-Type':urlInfo.mime
+				});
+				outputFiles(pathnames,res);
+			}
+		});
+		
+	}).listen(port,function(){
+		console.log('running on port 3000!!');
+		});
 }
 
-function main() {
-			root = '.';
-		    http.createServer(function (request, response) {
-					        var urlInfo = parseURL(root, request.url);
+function validateFiles(pathnames,callback){
+	(function next(i,len){
+		if(i < len){
+			fs.stat(pathnames[i],function(err,stats){
+				if(err)
+				{
+					callback(err);
+				}
+				else
+				{
+					i++;
+					next(i+1,len);
+				}
+			});
+		}
+		else
+		{
+			callback(null,pathnames);
+		}
+	}(0,pathnames.length));
+}
 
-							        combineFiles(urlInfo.pathnames, function (err, data) {
-										            if (err) {
-													                response.writeHead(404);
-																	                response.end(err.message);
-																					            } else {
-																								                response.writeHead(200, {
-																													                    'Content-Type': urlInfo.mime
-																																		                });																												                response.end(data);} });}).listen(3000);}
+function outputFiles(pathnames,writer)
+{
+	(function next(i,len){
+		if(i<len)
+		{
+			var reader = fs.createReadStream(pathnames[i]);
+			reader.pipe(writer,{end:false});
+			reader.on('end',function(){
+				next(i+1,len);
+			});
+		}
+		else
+		{
+			writer.end();
+		}
+	}(0,pathnames.length));
+}
 
 function parseURL(root, url) {
-	    var base, pathnames, parts;
-			console.log(url);
-		    if (url.indexOf('??') === -1) {
-				        url = url.replace('/', '/??');
-						    }
-			console.log(url);
+    var base, pathnames, parts;
 
-			    parts = url.split('??');
-			console.log(parts);
-				    base = parts[0];
-					    pathnames = parts[1].split(',').map(function (value) {
-								        return path.join(root, base, value);
-										    });
+    if (url.indexOf('??') === -1) {
+        url = url.replace('/', '/??');
+    }
 
-						    return {
-								        mime: MIME[path.extname(pathnames[0])] || 'text/plain',
-												          pathnames: pathnames
-															      };
+    parts = url.split('??');
+    base = parts[0];
+    pathnames = parts[1].split(',').map(function (value) {
+        return path.join(root, base, value);
+    });
+
+    return {
+        mime: MIME[path.extname(pathnames[0])] || 'text/plain',
+        pathnames: pathnames
+    };
 }
 
-main();
+main(process.argv.slice(2));
